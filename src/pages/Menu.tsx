4b40e7eb0d73c551dcd96dc22c840cee23fce7c5
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-09-07 17:41:49 +0530

    feat: update z-index values for Navbar and content in Menu page for improved layering

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -6,13 +6,17 @@ const Menu = () => {
   return (
     <>
       <div className="relative w-full  h-screen">
-        <div className="absolute top-0 left-0 w-full z-30">
+        <div className="absolute top-0 left-0 w-full z-50">
           <Navbar />
         </div>
         <div className="absolute inset-0 z-10">
           <img className="w-full h-full object-cover" src={assets.menucover} alt="" />
         </div>
         <div className="absolute inset-0 z-20 bg-black/50 "></div>
+        <div className="px-[100px]  relative flex flex-col h-full items-start justify-center z-50">
+          <h1 className="text-8xl max-w-[400px] text-white font-rufina">View our new menu</h1>
+          <p className="mt-[15px] text-2xl text-white font-lato">The freshest ingredients for you every day</p>
+        </div>
       </div>
       {/*first section*/}
       <div className="px-[50px]">
