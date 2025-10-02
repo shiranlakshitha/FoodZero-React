0b8c66dae827fd4c9f62c81f6ab145ce41b51d5a
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-10-02 07:13:34 +0530

    feat: enhance PortfolioCard styling for improved visual hierarchy and responsiveness

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -2,16 +2,19 @@ import { FaArrowRight } from "react-icons/fa";
 
 const PortfolioCard = ({ item }: { item: { name: string; meal: string; type: string; image: string } }) => {
   return (
-    <div className="w-full max-h-[600px] h-full overflow-hidden relative">
+    <div className="w-full text-white max-h-[600px] h-full overflow-hidden relative">
       <div className="w-full h-full">
         <img className="w-full h-full object-cover" src={item.image} alt="" />
       </div>
-      <div className="absolute bottom-5 w-full px-[25px] flex flex-row justify-between gap-5 items-center">
+
+      <div className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-black/70 to-transparent z-10 pointer-events-none" />
+
+      <div className="z-10 absolute bottom-[30px] w-full px-[25px] flex flex-row justify-between gap-5 items-center">
         <div className="flex flex-col gap-4">
-          <h3 className="font-rufina text-xl">{item.name}</h3>
-          <div className="flex flex-row gap-5 font-lato text-[12px]">
+          <h3 className="font-rufina text-xl md:text-3xl">{item.name}</h3>
+          <div className="flex flex-row gap-5 font-lato md:text-xl text-[12px]">
             <p>{item.meal}</p>
-            <p>&#x2022;{item.type}</p>
+            <p className="cursor-pointer">&#x2022;{item.type}</p>
           </div>
         </div>
         <div><FaArrowRight /></div>
