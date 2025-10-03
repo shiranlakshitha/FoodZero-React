535af07c811a0dcb54669478d90088fb37404f95
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-10-03 09:27:31 +0530

    feat: add framer-motion for enhanced animations on Home page components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ import TypesCard from "../components/TypesCard";
 import RatingsCard from "../components/RatingsCard";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import { motion } from "framer-motion";
 
 export interface menuItem {
   price: number;
@@ -23,6 +24,9 @@ export interface foodType {
 }
 
 const Home = () => {
+
+
+  
   const menu: menuItem[] = [
     {
       price: 20,
@@ -89,32 +93,67 @@ const Home = () => {
     <div className="">
       <div className="bg-[#233000] h-[1800px] px-[50px]">
         <div className="pt-[50px] relative w-full h-auto lg:h-[750px]">
-          <div className="max-md:py-[50px] max-lg:text-center absolute top-0 left-0 max-w-[1000px] z-60 text-white">
-            <h1 className="font-rufina max-lg:mt-[35px] text-[80px] max-sm:text-[60px] md:text-[100px] lg:text-[140px] leading-tight  ">
+          <motion.div 
+          initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          className="max-md:py-[50px] max-lg:text-center absolute top-0 left-0 max-w-[1000px] z-60 text-white">
+            <h1
+               
+            className="font-rufina max-lg:mt-[35px] text-[80px] max-sm:text-[60px] md:text-[100px] lg:text-[140px] leading-tight  ">
               Healthy Eating is important part of lifestyle
             </h1>
-            <p className="mt-[25px] font-lato text-lg max-md:self-center w-full lg:max-w-[500px] ">
+            <p
+              
+              className="mt-[25px] font-lato text-lg max-md:self-center w-full lg:max-w-[500px] "
+            >
               Discover the benefits of a balanced diet and how it can improve
               your overall well-being.
             </p>
-          </div>
+          </motion.div>
           <div className="relative flex items-center lg:absolute bottom-0 right-0 h-[600px] w-full lg:w-[600px] z-10">
-            <img
+            <motion.img
               src={assets.heroimg1}
               className="w-full h-full object-cover"
               alt=""
+              initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
             />
           </div>
           {/*overlay*/}
           <div className="absolute hidden max-lg:block mt-[50px] h-[600px] left-0 top-0 w-full lg:w-[600px] bg-black opacity-50 z-30"></div>
           <div className="absolute hidden lg:flex flex-row gap-[25px] z-30 bottom-[-50px] right-[350px]">
-            <img src={assets.spices1} className="w-[150px]" alt="" />
-            <img src={assets.spices2} className="w-[150px]" alt="" />
-            <img src={assets.spices3} className="w-[150px]" alt="" />
+            <motion.img
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            src={assets.spices1} className="w-[150px]" alt="" />
+            <motion.img
+              src={assets.spices2}
+              className="w-[150px]"
+              alt=""
+              initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            />
+            <motion.img
+              src={assets.spices3}
+              className="w-[150px]"
+              alt=""
+              initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            />
           </div>
         </div>
         <div className=" py-[25px] lg:mt-[150px] flex gap-5  flex-row max-md:flex-col justify-around">
-          <div className="flex flex-col items-start flex-2/3">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="flex flex-col items-start flex-2/3"
+          >
             <img
               src={assets.heroimg2}
               className="max-w-[600px] h-full w-full object-cover"
@@ -127,8 +166,13 @@ const Home = () => {
               Take the first step towards a healthier you by creating a
               personalized meal plan.
             </p>
-          </div>
-          <div className="flex max-md:flex-col-reverse flex-col flex-1/3">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            className="flex max-md:flex-col-reverse flex-col flex-1/3"
+          >
             <p className="text-white">
               Explore our wide range of healthy recipes and start your journey
               towards a balanced diet.
@@ -138,7 +182,7 @@ const Home = () => {
               className="max-w-[600px] h-full w-full object-cover"
               alt=""
             />
-          </div>
+          </motion.div>
         </div>
       </div>
       {/*third section*/}
@@ -158,18 +202,29 @@ const Home = () => {
         </div>
         <div className="mt-[50px] grid grid-cols-1 md:grid-cols-2 md:gap-9 gap-6 mb-[50px] md:gap-y-[100px]">
           {menu.map((item, index) => (
-            <HomeMenus
+            <motion.div
               key={index}
-              price={item.price}
-              title={item.title}
-              description={item.description}
-            />
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: index * 0.2 }}
+            >
+              <HomeMenus
+                key={index}
+                price={item.price}
+                title={item.title}
+                description={item.description}
+              />
+            </motion.div>
           ))}
         </div>
       </div>
       {/*fourth section*/}
       <div className="flex flex-col h-auto lg:h-[710px] md:flex-row py-[100px] pt-[50px] px-[50px] bg-[#EBF0E4] gap-5 relative">
-        <div className="flex-1">
+        <motion.div className="flex-1"
+        initial={{ opacity: 0, x: -50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}
+        >
           <img
             src={assets.chef}
             className="lg:hidden block object-cover w-full h-auto"
@@ -182,8 +237,12 @@ const Home = () => {
               alt=""
             />
           </div>
-        </div>
-        <div className="flex-1 items-center mx-auto mt-[25px] lg:ml-[100px] ">
+        </motion.div>
+        <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        className="flex-1 items-center mx-auto mt-[25px] lg:ml-[100px] ">
           <h1 className="text-3xl md:text-7xl font-rufina lg:max-w-[200px] leading-25">
             Excellent Cook
           </h1>
@@ -195,11 +254,17 @@ const Home = () => {
             every meal is crafted to perfection, offering a delightful dining
             experience that promotes a healthy lifestyle.
           </p>
-        </div>
+        </motion.div>
       </div>
       {/*fifth section*/}
       <div className="px-[50px] py-[50px] md:py-[75px] grid-cols-1 md:grid-cols-3 gap-3 grid">
         {qualities.map((item, index) => (
+          <motion.div 
+            key={index}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: index * 0.2 }}
+          >
           <div
             key={index}
             className="flex flex-col text-center justify-start  items-center"
@@ -214,11 +279,18 @@ const Home = () => {
               <p className="font-lato">{item.description}</p>
             </div>
           </div>
+          </motion.div>
         ))}
       </div>
       {/*sixth section*/}
       <div className="py-[50px] grid grid-cols-1 md:grid-cols-2 px-[50px] justify-items-center gap-5">
-        {blogs.map((item, index) => (
+        {blogs.slice(0, 2).map((item, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, delay: index * 0.2 }}
+          >
           <BlogCard
             key={index}
             title={item.title}
@@ -230,29 +302,59 @@ const Home = () => {
             time={item.time}
             comments={item.comments}
           />
+          </motion.div>
         ))}
       </div>
       {/*seventh section*/}
-      <Reservations />
+      <motion.div
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+      ><Reservations /></motion.div>
+      
+
       {/*eightth section*/}
       <div className="text-center px-[50px] py-[50px]">
-        <h1 className="text-5xl font-semibold  font-rufina text-black">
+        <motion.h1
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="text-5xl font-semibold  font-rufina text-black"
+        >
           Calories Energy Balance
-        </h1>
-        <p className="mt-[35px] font-lato max-w-[600px] mx-auto">
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          className="mt-[35px] font-lato max-w-[600px] mx-auto"
+        >
           Maintaining a healthy weight requires balancing the calories you
           consume with the calories you burn. Our menu is designed to help you
           achieve that balance with delicious, nutritious options.
-        </p>
+        </motion.p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-[50px] justify-items-center">
           {foodTypes.map((item, index) => (
-            <TypesCard key={index} img={item.img} type={item.type} />
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 30 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, delay: index * 0.2 }}
+            >
+              <TypesCard key={index} img={item.img} type={item.type} />
+            </motion.div>
           ))}
         </div>
       </div>
       {/*ninth section*/}
       <div className="px-[50px] py-[100px]">
-        <RatingsCard />
+        <motion.div
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <RatingsCard />
+        </motion.div>
       </div>
       {/*tenth section*/}
       <Footer />
